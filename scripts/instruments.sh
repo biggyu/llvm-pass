8c6d6c8fad54fb52fd753c8f59f9fcf9a65ef540
Seung Gyu Lee <sl2486@ilab1.cs.rutgers.edu>
2026-04-15 12:44:06 -0400

    chore: compile, execute

--- scripts/instruments.sh
+++ scripts/instruments.sh
@@ -16,9 +16,16 @@ mkdir -p "$OUTDIR"
 
 # -ffp-contract=off prevents llvm.fmuladd instructions
 # -fno-math-errno prevents llvm.sqrt instructions
-# $LLVM_CLANG -O2 -g -S -fno-math-errno -emit-llvm $SRC -o $OUTDIR/input.ll
+# $LLVM_CLANG -O0 -g -S -fno-math-errno -emit-llvm $SRC -o $OUTDIR/input.ll
 $LLVM_CLANG -O0 -g -S -emit-llvm -ffp-contract=off $SRC -o $OUTDIR/input.ll
-$LLVM_OPT -load-pass-plugin ./build/passes/$PASS/$PASS.so --passes="$PLUGIN" -S $OUTDIR/input.ll -o $OUTDIR/instrumented.ll
-$LLVM_CLANGXX -c $OUTDIR/instrumented.ll -o $OUTDIR/instrumented.o
-g++ $OUTDIR/instrumented.o ./build/runtime/libpass_runtime.a -o $OUTDIR/a.out -lm -lmpfr -lgmp
+$LLVM_OPT \
+    -load-pass-plugin ./build/passes/$PASS/$PASS.so \
+    --passes="$PLUGIN" \
+    -S $OUTDIR/input.ll \
+    -o $OUTDIR/instrumented.ll
+$LLVM_CLANGXX \
+    -c $OUTDIR/instrumented.ll \
+    -o $OUTDIR/instrumented.o
+g++ $OUTDIR/instrumented.o ./build/runtime/libpass_runtime.a \
+    -o $OUTDIR/a.out -lm -lmpfr -lgmp
 $OUTDIR/a.out
