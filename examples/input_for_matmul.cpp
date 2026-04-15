0f838430f5a66548da628c5a4f5dd4624a5debc3
Seung Gyu Lee <sl2486@ilab1.cs.rutgers.edu>
2026-04-15 12:42:43 -0400

    test: example code modified

---
+++ examples/input_for_matmul.cpp
@@ -0,0 +1,74 @@
+// #include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <chrono>
+
+template <typename T>
+static double run_matmul(int dim, int rep) {
+    std::vector<std::vector<T>> mat1(dim, std::vector<T>(dim));
+    std::vector<std::vector<T>> mat2(dim, std::vector<T>(dim));
+    std::vector<std::vector<T>> res(dim, std::vector<T>(dim, 0.0));
+
+    srand(0);
+    for (int i = 0; i < dim; i++) {
+        for (int j = 0; j < dim; j++) {
+            mat1[i][j] = ((T)rand()) / ((T)RAND_MAX + 1);
+            mat2[i][j] = ((T)rand()) / ((T)RAND_MAX + 1);
+        }
+    }
+
+    auto start = std::chrono::steady_clock::now();
+    for (int k = 0; k < rep; k++) {
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
+                res[i][j] = 0;
+                for (int k = 0; k < dim; k++) {
+                    res[i][j] += mat1[i][k] * mat2[k][j];
+                }
+            }
+        }
+    }
+    auto end = std::chrono::steady_clock::now();
+
+    double checksum = 0.0;
+    for (int i = 0; i < dim; i++) {
+        for (int j = 0; j < dim; j++) {
+            checksum += res[i][j];
+        }
+    }
+    auto tot = end - start;
+    return std::chrono::duration<double, std::milli>(tot).count();
+}
+int main() {
+    static int dim = 100;
+    static int warmup = 3;
+    static int rep = 500;
+    auto total_time = 0;
+
+    run_matmul<double>(dim, warmup);
+    // std::cout << "Double type matmul execution time: " 
+    //             << std::chrono::duration<double, std::milli>(total_time).count() << "ms" << std::endl;
+    // std::cout << "Checksum: " << checksum << std::endl;
+
+    total_time = run_matmul<double>(dim, rep);
+    printf("Double type matmul exeuction time: %fms\n", std::chrono::duration<double, std::milli>(total_time).count());
+    // std::cout << "Double type matmul execution time: " 
+    //             << std::chrono::duration<double, std::milli>(total_time).count() << "ms" << std::endl;
+    // // std::cout << "Checksum: " << checksum << std::endl;
+
+    // auto total_time = end - start;
+
+    // run_matmul<float>(dim, warmup);
+    // std::cout << "Float type matmul execution time: " 
+    //             << std::chrono::duration<double, std::milli>(total_time).count() << "ms" << std::endl;
+    // // std::cout << "Checksum: " << checksum << std::endl;
+    
+    total_time = run_matmul<float>(dim, rep);
+    printf("Float type matmul exeuction time: %fms\n", std::chrono::duration<double, std::milli>(total_time).count());
+    // std::cout << "Float type matmul execution time: " 
+    //             << std::chrono::duration<double, std::milli>(total_time).count() << "ms" << std::endl;
+
+    // report_fp_profile();
+    // report_smem_profile();
+}
--- examples/input_for_twosum.c
+++ examples/input_for_twosum.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void) {
     float  f1 = 1.0f, f2 = 1e-7f;
@@ -7,19 +8,29 @@ int main(void) {
     double addd = a + b;
     double subd = a - b;
     double muld = a * b;
-    double result1d = muld - addd - subd;
-    double result2d = -1 * a * b;
+    // double result1d = muld - addd + subd;
+    // double result2d = -1 * a / b;
+    double result3d = sqrt(muld) * sqrt(addd) / sqrt(subd);
 
     float addf = f1 + f2;
     float subf = f1 - f2;
     float mulf = f1 * f2;
-    float result1f = mulf - addf - subf;
-    float result2f = -1 * f1 * f2;
+    // float result1f = mulf - addf - subf;
+    // float result2f = -1 * f1 / f2;
+    float result3f = sqrtf(mulf) * sqrtf(addf) / sqrtf(subf);
 
-    // printf("add = %f\n", add);
-    // printf("sub = %f\n", sub);
-    // printf("mul = %f\n", mul);
-    // printf("result1 = %f\n", result1);
-    // printf("result2 = %f\n", result2);
+    printf("add = %f\n", addd);
+    printf("sub = %f\n", subd);
+    printf("mul = %f\n", muld);
+    // printf("result1 = %f\n", result1d);
+    // printf("result2 = %f\n", result2d);
+    printf("result3 = %f\n", result3d);
+
+    printf("add = %f\n", addf);
+    printf("sub = %f\n", subf);
+    printf("mul = %f\n", mulf);
+    // printf("result1 = %f\n", result1f);
+    // printf("result2 = %f\n", result2f);
+    printf("result3 = %f\n", result3f);
     return 0;
 }
