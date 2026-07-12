cf69d71ebba168d2341e3c2c3f6f560545fdc6ca
seunggyu <sgkevinlee@naver.com>
2026-07-12 03:56:50 -0400

    gitignore updated

--- benchmarks/correctness_test/src/compare-zero-self.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() {
-  volatile double x;
-  int cmp;
-  x = 0.0;
-  cmp = (x == 0.0);
-  printf("%d\n", cmp);
-}
--- benchmarks/correctness_test/src/tiny.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-// extern void eftsan_print_error(double);
-int main() {
-  int x;
-  x = 1 + 2;
-  printf("%d\n", x);
-  // eftsan_print_error(x);
-  return 0;
-}
