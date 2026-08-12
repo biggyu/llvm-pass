fbd89ec47b4b533c2ba279ab2a363d45a5917bd5
seunggyu <sgkevinlee@naver.com>
2026-08-12 12:44:10 -0400

    test: controleed test expected logs

--- tests/controlled/cases/log_one.c
+++ tests/controlled/cases/log_one.c
@@ -3,8 +3,9 @@
 
 int main() {
     volatile double acc = 0;
+    volatile double one = 1.0;
     for (double x = 0; x < 1000; x++) {
-        acc += log(1.0);
+        acc += log(one);
     }
     printf("%g\n", acc);
     return 0;
